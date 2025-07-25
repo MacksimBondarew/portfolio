7e921917b4d02fe2b9b14e34270d9158cda8b66a
MacksimBondarew <112969071+MacksimBondarew@users.noreply.github.com>
2025-07-25 13:40:57 +0200

    fix: animation for mobile menu

--- portfolio/src/components/layout/header/navigation.tsx
+++ portfolio/src/components/layout/header/navigation.tsx
@@ -3,28 +3,30 @@
 import React, { useState } from "react";
 import { Icon, SocialLinks } from "@/components/shared";
 import HeaderLinks from "./headerLinks";
-import { socialLinks } from "../../data";
 
 export default function Navigation() {
     const [isOpen, setIsOpen] = useState(false);
     return (
         <nav>
             <HeaderLinks className="tablet" />
-            <button onClick={() => setIsOpen((prevState) => !prevState)}>
-                {isOpen ? (
-                    <Icon width={24} height={24} iconId="icon-close" />
-                ) : (
-                    <Icon width={24} height={24} iconId="icon-burger" />
-                )}
+            <button
+                onClick={() => setIsOpen((prevState) => !prevState)}
+                className="transition-transform duration-[250ms] ease-in-out md:hidden">
+                <Icon
+                    width={24}
+                    height={24}
+                    iconId={isOpen ? "icon-close" : "icon-burger"}
+                    styles={{
+                        transform: isOpen ? "rotate(90deg)" : "rotate(0deg)",
+                    }}
+                />
             </button>
-            {isOpen && (
-                <div className="open">
-                    <HeaderLinks className="mobile" />
-                    <ul>
-                        <SocialLinks />
-                    </ul>
-                </div>
-            )}
+            <div className={isOpen ? "open" : ""}>
+                <HeaderLinks className="mobile" />
+                <ul>
+                    <SocialLinks />
+                </ul>
+            </div>
         </nav>
     );
 }
